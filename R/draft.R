10ec6de064d5bd0b294ee79c15d2b69ccdd95afc
Lilia Medina <abymed27@gmail.com>
2026-05-22 21:43:14 +0200

    deleted  because i decided to continue with targets instead

--- R/draft.R
+++ R/draft.R
@@ -12,31 +12,6 @@ load_data <- function(file) {
     janitor::remove_empty()
 }
 
-##Expectation/Validation
-
-validate_patients <- function(patients) {
-  patients |> 
-    col_vals_between(
-      birthdate,
-      as.Date("1900-01-01"),
-      Sys.Date(),
-      label = "Birthdate",
-      na_pass = TRUE
-    ) |>
-    #Birth before death
-    col_vars_gte(
-      deathdate,
-      vars(birthdate),
-      label = "Death data after birth date",
-      na_pass = TRUE
-    ) |>
-    #SSN check
-    col_vals_regenx(
-      ssn, 
-    "[0-9]{3}-[0-9]{2}-[0-9]{4}$",
-      label = "SSN must follow XXX-XX-XXXX format"
-    ) |>
-  }
   
     
 
